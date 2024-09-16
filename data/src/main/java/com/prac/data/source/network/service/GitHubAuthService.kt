3a3be561913abec2addedc052f84d5c5952cf3d0
GongDoMin <yhw198@naver.com>
2024-09-16 11:01:35 +0900

    feat: token 을 갱신하기 위해 service 에 refreshToken 메서드 추가

--- data/src/main/java/com/prac/data/source/network/service/GitHubAuthService.kt
+++ data/src/main/java/com/prac/data/source/network/service/GitHubAuthService.kt
@@ -14,4 +14,13 @@ internal interface GitHubAuthService {
         @Query("client_secret") clientSecret: String = BuildConfig.CLIENT_SECRET,
         @Query("code") code: String,
     ): TokenDto
+
+    @POST("login/oauth/access_token")
+    suspend fun refreshToken(
+        @Header("Accept") accept: String = "application/json",
+        @Query("client_id") clientID: String = BuildConfig.CLIENT_ID,
+        @Query("client_secret") clientSecret: String = BuildConfig.CLIENT_SECRET,
+        @Query("grant_type") grantType: String = "refresh_token",
+        @Query("refresh_token") refreshToken: String
+    ): TokenDto
 }
