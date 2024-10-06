5c6f56ef82a468c89a669047e2629b859d3a873b
GongDoMin <yhw198@naver.com>
2024-10-06 16:13:13 +0900

    feat: TokenLocalDataSource 구현체에서 cahced token 생성 및 init 에서 초기화

--- data/src/main/java/com/prac/data/source/local/impl/TokenLocalDataSourceImpl.kt
+++ data/src/main/java/com/prac/data/source/local/impl/TokenLocalDataSourceImpl.kt
@@ -1,14 +1,26 @@
 package com.prac.data.source.local.impl
 
 import com.prac.data.source.local.datastore.TokenDataStoreManager
-import com.prac.data.repository.model.TokenModel
 import com.prac.data.source.local.TokenLocalDataSource
 import com.prac.data.source.local.datastore.TokenLocalDto
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 
 internal class TokenLocalDataSourceImpl @Inject constructor(
     private val tokenDataStoreManager: TokenDataStoreManager
 ) : TokenLocalDataSource {
+
+    private val cachedToken: AtomicReference<TokenLocalDto> = AtomicReference()
+
+    init {
+        runBlocking {
+            val token = tokenDataStoreManager.getToken()
+
+            cachedToken.set(token)
+        }
+    }
+
     override suspend fun setToken(token: TokenLocalDto) {
         tokenDataStoreManager.saveTokenData(token)
     }
