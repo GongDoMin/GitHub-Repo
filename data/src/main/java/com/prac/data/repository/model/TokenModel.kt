386918aeb5608f60ca69a72cedb5bf713c50de21
GongDoMin <yhw198@naver.com>
2024-10-06 15:55:01 +0900

    refact: TokenModel 멤버 함수 제거

--- data/src/main/java/com/prac/data/repository/model/TokenModel.kt
+++ data/src/main/java/com/prac/data/repository/model/TokenModel.kt
@@ -8,10 +8,4 @@ internal data class TokenModel(
     val expiresInMinute: Int,
     val refreshTokenExpiresInMinute: Int,
     val updatedAt: ZonedDateTime
-) {
-    val isExpired: Boolean
-        get() = updatedAt.plusMinutes(expiresInMinute.toLong()).isBefore(ZonedDateTime.now())
-
-    val isRefreshTokenExpired: Boolean
-        get() = updatedAt.plusMinutes(refreshTokenExpiresInMinute.toLong()).isBefore(ZonedDateTime.now())
-}
+)
