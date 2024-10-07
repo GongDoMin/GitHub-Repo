1251c8d1354834549b36de777b1fa3455e3fb661
GongDoMin <yhw198@naver.com>
2024-10-07 14:09:09 +0900

    feat: 레파지토리가 유효하지 않을 때 사용되는 상수 추가

--- app/src/main/java/com/prac/githubrepo/constants/ErrorMessage.kt
+++ app/src/main/java/com/prac/githubrepo/constants/ErrorMessage.kt
@@ -3,3 +3,4 @@ package com.prac.githubrepo.constants
 const val LOGIN_FAIL = "로그인을 실패했습니다."
 const val CONNECTION_FAIL = "연결에 실패했습니다."
 const val INVALID_TOKEN = "토큰이 만료되었습니다. 다시 로그인해주세요."
+const val INVALID_REPOSITORY = "유효하지 않는 레파지토리입니다."
