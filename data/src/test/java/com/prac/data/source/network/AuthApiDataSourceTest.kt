03cf54761356de25113a493ea3a8420a684aafa0
GongDoMin <yhw198@naver.com>
2024-10-10 20:28:15 +0900

    feat: OAuth 인증 기능 테스트 추가

--- data/src/test/java/com/prac/data/source/network/AuthApiDataSourceTest.kt
+++ data/src/test/java/com/prac/data/source/network/AuthApiDataSourceTest.kt
@@ -1,11 +1,16 @@
 package com.prac.data.source.network
 
+import com.prac.data.source.network.dto.TokenDto
 import com.prac.data.source.network.impl.AuthApiDataSourceImpl
 import com.prac.data.source.network.service.GitHubAuthService
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 internal class AuthApiDataSourceTest {
@@ -16,5 +21,19 @@ internal class AuthApiDataSourceTest {
     @Before
     fun setup() {
         authApiDataSource = AuthApiDataSourceImpl(gitHubAuthService)
+    }
+
+    @Test
+    fun authorizeOAuth_tokenPassedToDataSource() = runTest {
+        val code = "code"
+        val tokenDto = TokenDto("accessToken", 3600, "refreshToken", 3600, "Bearer")
+        whenever(gitHubAuthService.authorizeOAuth(code = code)).thenReturn(tokenDto)
+
+        val result = authApiDataSource.authorizeOAuth(code)
+
+        assertEquals(result.accessToken, tokenDto.accessToken)
+        assertEquals(result.refreshToken, tokenDto.refreshToken)
+        assertEquals(result.expiresInSeconds, tokenDto.expiresIn)
+        assertEquals(result.refreshTokenExpiresInSeconds, tokenDto.refreshTokenExpiresIn)
     }
 }
