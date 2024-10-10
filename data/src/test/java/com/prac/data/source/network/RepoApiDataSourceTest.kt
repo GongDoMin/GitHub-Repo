1293ce1affde4b7799583836a59e7a1182291b5a
GongDoMin <yhw198@naver.com>
2024-10-10 20:43:36 +0900

    feat: repositories 목록 가져오기 기능 테스트 추가

--- data/src/test/java/com/prac/data/source/network/RepoApiDataSourceTest.kt
+++ data/src/test/java/com/prac/data/source/network/RepoApiDataSourceTest.kt
@@ -1,11 +1,17 @@
 package com.prac.data.source.network
 
+import com.prac.data.source.network.dto.OwnerDto
+import com.prac.data.source.network.dto.RepoDto
 import com.prac.data.source.network.impl.RepoApiDataSourceImpl
 import com.prac.data.source.network.service.GitHubService
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class RepoApiDataSourceTest {
@@ -16,5 +22,29 @@ class RepoApiDataSourceTest {
     @Before
     fun setUp() {
         repoApiDatasource = RepoApiDataSourceImpl(gitHubService)
+    }
+
+    @Test
+    fun getRepositories_repositoriesPassedToDataSource() = runTest {
+        val repositoriesDto = listOf(
+            RepoDto(0, "test1", OwnerDto("test1", "test1"), 0, "test1"),
+            RepoDto(1, "test2", OwnerDto("test2", "test2"), 0, "test2"),
+            RepoDto(2, "test3", OwnerDto("test3", "test3"), 0, "test3"),
+        )
+        val repoName = "test"
+        val perPage = 10
+        val page = 1
+        whenever(gitHubService.getRepos(repoName, perPage, page)).thenReturn(repositoriesDto)
+
+        val result = repoApiDatasource.getRepositories(repoName, perPage, page)
+
+        result.indices.forEach {
+            assertEquals(result[it].id, repositoriesDto[it].id)
+            assertEquals(result[it].name, repositoriesDto[it].name)
+            assertEquals(result[it].owner.login, repositoriesDto[it].owner.login)
+            assertEquals(result[it].owner.avatarUrl, repositoriesDto[it].owner.avatarUrl)
+            assertEquals(result[it].stargazersCount, repositoriesDto[it].stargazersCount)
+            assertEquals(result[it].updatedAt, repositoriesDto[it].updatedAt)
+        }
     }
 }
