e6b72ecb2b3559b98e9f7a5cf26d8874ed5e9e0a
GongDoMin <yhw198@naver.com>
2024-10-11 12:28:38 +0900

    feat: insertRepositories 함수 테스트 코드 구현

--- data/src/androidTest/java/com/prac/data/source/local/RepositoryDaoTest.kt
+++ data/src/androidTest/java/com/prac/data/source/local/RepositoryDaoTest.kt
@@ -1,12 +1,18 @@
 package com.prac.data.source.local
 
+import androidx.paging.PagingSource
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.prac.data.source.local.room.dao.RepositoryDao
 import com.prac.data.source.local.room.database.RepositoryDatabase
+import com.prac.data.source.local.room.entity.Owner
+import com.prac.data.source.local.room.entity.Repository
+import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -27,4 +33,22 @@ class RepositoryDaoTest {
     fun tearDown() {
         repositoryDatabase.close()
     }
+
+    @Test
+    fun insertRepositories_insertItemsIntoDatabase() = runTest {
+        val repositories = makeRepositories()
+
+        repositoryDao.insertRepositories(repositories)
+
+        val result = (repositoryDao.getRepositories().load(
+            PagingSource.LoadParams.Refresh(key = null, loadSize = 10, placeholdersEnabled = false)
+        ) as? PagingSource.LoadResult.Page)?.data
+        assertEquals(result, repositories)
+    }
+
+    private fun makeRepositories() =
+        listOf(
+            Repository(1, "repo1", Owner("test1", "test1"), 0, "test1", false),
+            Repository(2, "repo2", Owner("test2", "test2"), 0, "test2", false)
+        )
 }
