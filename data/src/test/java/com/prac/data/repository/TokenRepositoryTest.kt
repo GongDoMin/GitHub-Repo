0b90083b313f153660bb3cafc479a1d3d0b2136c
GongDoMin <yhw198@naver.com>
2024-10-11 11:33:35 +0900

    feat: TokenRepository 의 authorizeOAuth 성공 테스트 구현

--- data/src/test/java/com/prac/data/repository/TokenRepositoryTest.kt
+++ data/src/test/java/com/prac/data/repository/TokenRepositoryTest.kt
@@ -1,12 +1,20 @@
 package com.prac.data.repository
 
 import com.prac.data.repository.impl.TokenRepositoryImpl
+import com.prac.data.repository.model.TokenModel
 import com.prac.data.source.local.TokenLocalDataSource
+import com.prac.data.source.local.datastore.TokenLocalDto
 import com.prac.data.source.network.AuthApiDataSource
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import java.time.ZonedDateTime
 
 @RunWith(MockitoJUnitRunner::class)
 class TokenRepositoryTest {
@@ -22,5 +30,26 @@ class TokenRepositoryTest {
             tokenLocalDataSource = tokenLocalDataSource,
             authApiDataSource = authApiDataSource
         )
+    }
+
+    @Test
+    fun authorizeOAuth_updateDataStoreAndReturnSuccess() = runTest {
+        val code = "testCode"
+        val token = TokenModel("accessToken", "refreshToken", 3600, 3600, ZonedDateTime.now())
+        whenever(authApiDataSource.authorizeOAuth(code)).thenReturn(token)
+
+        val result = tokenRepository.authorizeOAuth(code)
+
+        verify(tokenLocalDataSource)
+            .setToken(
+                TokenLocalDto(
+                    token.accessToken,
+                    token.refreshToken,
+                    token.expiresInSeconds,
+                    token.refreshTokenExpiresInSeconds,
+                    token.updatedAt
+                )
+            )
+        assertTrue(result.isSuccess)
     }
 }
