f1d6871faddca652c4892102b1800c69de05b052
GongDoMin <yhw198@naver.com>
2024-10-11 11:13:53 +0900

    feat: data store 가 비어있을 때 token 이 empty 판단하는 기능 테스트 추가

--- data/src/test/java/com/prac/data/source/local/TokenLocalDataSourceTest.kt
+++ data/src/test/java/com/prac/data/source/local/TokenLocalDataSourceTest.kt
@@ -1,11 +1,18 @@
 package com.prac.data.source.local
 
 import com.prac.data.source.local.datastore.TokenDataStoreManager
+import com.prac.data.source.local.datastore.TokenLocalDto
+import com.prac.data.source.local.impl.TokenLocalDataSourceImpl
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
+import java.time.Instant
+import java.time.ZoneId
 
 @RunWith(MockitoJUnitRunner::class)
 internal class TokenLocalDataSourceTest {
@@ -16,5 +23,25 @@ internal class TokenLocalDataSourceTest {
     @After
     fun tearDown() = runTest {
         tokenLocalDataSource.clearToken()
+    }
+
+    @Test
+    fun getCachedToken_returnEmptyTokenWhenDataStoreIsEmpty() = runTest {
+        whenever(tokenDataStoreManager.getToken())
+            .thenReturn(
+                TokenLocalDto(
+                    "",
+                    "",
+                    0,
+                    0,
+                    Instant.ofEpochMilli(0).atZone(ZoneId.systemDefault())
+                )
+            )
+        tokenLocalDataSource = TokenLocalDataSourceImpl(tokenDataStoreManager)
+
+        val result = tokenLocalDataSource.getToken()
+
+        assertTrue(result.accessToken.isEmpty())
+        assertTrue(result.refreshToken.isEmpty())
     }
 }
