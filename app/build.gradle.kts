ef713099c1dd7475c0820ff884588764a240b452
GongDoMin <yhw198@naver.com>
2024-10-12 19:59:07 +0900

    feat: test code 작성을 위한 dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,12 +63,15 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(project(":data"))
 
     implementation(libs.jetbrains.kotlinx.coroutines)
+    implementation(libs.jetbrains.kotlinx.coroutines.test)
 
     implementation(libs.glide)
 
