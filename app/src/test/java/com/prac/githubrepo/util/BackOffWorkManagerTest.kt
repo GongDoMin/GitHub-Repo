16d33c169e2c0a2d0bec8f0fa296b006c19d6082
GongDoMin <yhw198@naver.com>
2024-10-13 11:15:24 +0900

    feat: FakeBackOffWorkManager 클래스 생성 및 setUp, tearDown 메서드 구현

--- app/src/test/java/com/prac/githubrepo/util/BackOffWorkManagerTest.kt
+++ app/src/test/java/com/prac/githubrepo/util/BackOffWorkManagerTest.kt
@@ -1,5 +1,6 @@
 package com.prac.githubrepo.util
 
+import com.prac.data.exception.CommonException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -15,15 +16,82 @@ import java.io.IOException
 
 class BackOffWorkManagerTest {
 
-    private lateinit var backOffWorkManager: BackOffWorkManager
+    private lateinit var backOffWorkManager: FakeBackOffWorkManager
 
     @Before
     fun setUp() {
-
+        backOffWorkManager = FakeBackOffWorkManager()
     }
 
     @After
     fun tearDown() {
+        backOffWorkManager.clearWork()
+        backOffWorkManager.clearDelayTimes()
+    }
 
+    class FakeBackOffWorkManager : BackOffWorkManager {
+        private val _workMap = mutableMapOf<String, Job>()
+        private val _delayTimesMap = mutableMapOf<String, Long>()
+
+        private lateinit var scope: CoroutineScope
+
+        override fun addWork(
+            uniqueID: String,
+            times: Int,
+            initialDelay: Long,
+            maxDelay: Long,
+            factor: Double,
+            work: suspend () -> Result<*>
+        ) {
+            if (!::scope.isInitialized) {
+                throw Exception("scope not initialized")
+            }
+
+            removeWork(uniqueID)
+            _delayTimesMap.remove(uniqueID)
+
+            var currentDelay = initialDelay
+
+            val job = scope.launch {
+                repeat(times) {
+                    work()
+                        .onSuccess {
+                            removeWork(uniqueID)
+                            return@launch
+                        }
+                        .onFailure {
+                            if (it !is CommonException.NetworkError) {
+                                removeWork(uniqueID)
+                                return@launch
+                            }
+                        }
+                    _delayTimesMap[uniqueID] = (_delayTimesMap[uniqueID] ?: 0L) + currentDelay
+                    currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
+                }
+                removeWork(uniqueID)
+            }
+
+            _workMap[uniqueID] = job
+        }
+
+        override fun clearWork() {
+            _workMap.clear()
+        }
+
+        fun setScope(scope: CoroutineScope) {
+            this.scope = scope
+        }
+
+        fun clearDelayTimes() {
+            _delayTimesMap.clear()
+        }
+
+        fun getDelayTimes(uniqueID: String) : Long =
+            _delayTimesMap[uniqueID] ?: 0L
+
+        private fun removeWork(uniqueID: String) {
+            _workMap[uniqueID]?.cancel()
+            _workMap.remove(uniqueID)
+        }
     }
 }
