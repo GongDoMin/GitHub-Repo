953d1e7e797d4adbc5bfc6be7fcbb2c0228aa156
GongDoMin <yhw198@naver.com>
2024-10-14 20:37:32 +0900

    feat: 유효한 Repository 정보 입력 시 uiState 업데이트 확인

--- app/src/test/java/com/prac/githubrepo/main/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/prac/githubrepo/main/detail/DetailViewModelTest.kt
@@ -1,17 +1,25 @@
 package com.prac.githubrepo.main.detail
 
+import com.prac.data.entity.OwnerEntity
+import com.prac.data.entity.RepoDetailEntity
 import com.prac.data.repository.RepoRepository
 import com.prac.data.repository.TokenRepository
 import com.prac.githubrepo.util.FakeBackOffWorkManager
 import com.prac.githubrepo.util.StandardTestDispatcherRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
@@ -38,4 +46,44 @@ class DetailViewModelTest {
         backOffWork.clearWork()
         backOffWork.clearDelayTimes()
     }
+
+    @Test
+    fun getRepository_validInput_updatesUiStateToContent() = runTest {
+        val userName = "test"
+        val repoName = "test"
+        val repoDetailEntity = makeRepoDetailEntity()
+        val starStateAndCount = Pair(true, 11)
+        whenever(repoRepository.getRepository(userName, repoName))
+            .thenReturn(Result.success(repoDetailEntity))
+        whenever(repoRepository.getStarStateAndStarCount(repoDetailEntity.id))
+            .thenReturn(flow { emit(starStateAndCount) })
+
+        detailViewMock.getRepository(userName, repoName)
+        advanceUntilIdle()
+
+        val uiState = detailViewMock.uiState.value
+        val expectedValue = makeExpectedValue(starStateAndCount.first, starStateAndCount.second)
+        assertTrue(uiState is DetailViewModel.UiState.Content)
+        assertEquals((uiState as DetailViewModel.UiState.Content).repository, expectedValue)
+    }
+
+    private fun makeRepoDetailEntity() =
+        RepoDetailEntity(
+            id = 1,
+            name = "test",
+            owner = OwnerEntity(login = "test", avatarUrl = "test"),
+            stargazersCount = 10,
+            forksCount = 10,
+            isStarred = true
+        )
+
+    private fun makeExpectedValue(isStarred: Boolean, stargazersCount: Int) =
+        RepoDetailEntity(
+            id = 1,
+            name = "test",
+            owner = OwnerEntity(login = "test", avatarUrl = "test"),
+            stargazersCount = stargazersCount,
+            forksCount = 10,
+            isStarred = isStarred
+        )
 }
