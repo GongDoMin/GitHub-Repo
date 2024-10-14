f05dd7df2c83fa6df2126d85d5e55f76a937988d
GongDoMin <yhw198@naver.com>
2024-10-14 16:34:36 +0900

    feat: 로그인 성공 시 테스트 코드

--- app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
+++ app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
@@ -2,7 +2,9 @@ package com.prac.githubrepo.login
 
 import com.prac.data.repository.TokenRepository
 import com.prac.githubrepo.util.StandardTestDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Rule
@@ -31,5 +33,18 @@ class LoginViewModelTest {
         val result = loginViewModel.event.first()
 
         Assert.assertTrue(result is LoginViewModel.Event.Success)
+    }
+
+    @Test
+    fun loginWithGitHub_success_eventSuccess() = runTest {
+        val code = "code"
+        whenever(tokenRepository.isLoggedIn()).thenReturn(false)
+        loginViewModel = LoginViewModel(tokenRepository, standardTestDispatcherRule.testDispatcher)
+        whenever(tokenRepository.authorizeOAuth(code)).thenReturn(Result.success(Unit))
+
+        loginViewModel.loginWithGitHub(code)
+
+        val event = loginViewModel.event.first()
+        Assert.assertTrue(event is LoginViewModel.Event.Success)
     }
 }
