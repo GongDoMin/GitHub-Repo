e7c93228fc2e34e03120b72ae6016c08f1190213
GongDoMin <yhw198@naver.com>
2024-10-14 16:32:18 +0900

    feat: 자동로그인 테스트 코드

--- app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
+++ app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
@@ -2,10 +2,15 @@ package com.prac.githubrepo.login
 
 import com.prac.data.repository.TokenRepository
 import com.prac.githubrepo.util.StandardTestDispatcherRule
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class LoginViewModelTest {
@@ -17,4 +22,14 @@ class LoginViewModelTest {
     private lateinit var tokenRepository: TokenRepository
 
     private lateinit var loginViewModel: LoginViewModel
+
+    @Test
+    fun checkAutoLogin_userIsLoggedIn_eventSuccess() = runTest {
+        whenever(tokenRepository.isLoggedIn()).thenReturn(true)
+        loginViewModel = LoginViewModel(tokenRepository, standardTestDispatcherRule.testDispatcher)
+
+        val result = loginViewModel.event.first()
+
+        Assert.assertTrue(result is LoginViewModel.Event.Success)
+    }
 }
