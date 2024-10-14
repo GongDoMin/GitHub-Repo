2742c1e04a8011c1ed1c11ebebf03623eda29597
GongDoMin <yhw198@naver.com>
2024-10-14 16:39:40 +0900

    feat: 로그인 네트워크 실패 시 테스트 코드

--- app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
+++ app/src/test/java/com/prac/githubrepo/login/LoginViewModelTest.kt
@@ -1,12 +1,17 @@
 package com.prac.githubrepo.login
 
+import com.prac.data.exception.CommonException
 import com.prac.data.repository.TokenRepository
+import com.prac.githubrepo.constants.CONNECTION_FAIL
+import com.prac.githubrepo.main.MainViewModel
 import com.prac.githubrepo.util.StandardTestDispatcherRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,6 +19,7 @@ import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.whenever
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
 class LoginViewModelTest {
 
@@ -32,7 +38,7 @@ class LoginViewModelTest {
 
         val result = loginViewModel.event.first()
 
-        Assert.assertTrue(result is LoginViewModel.Event.Success)
+        assertTrue(result is LoginViewModel.Event.Success)
     }
 
     @Test
@@ -45,6 +51,21 @@ class LoginViewModelTest {
         loginViewModel.loginWithGitHub(code)
 
         val event = loginViewModel.event.first()
-        Assert.assertTrue(event is LoginViewModel.Event.Success)
+        assertTrue(event is LoginViewModel.Event.Success)
+    }
+
+    @Test
+    fun loginWithGitHub_networkError() = runTest {
+        val code = "code"
+        whenever(tokenRepository.isLoggedIn()).thenReturn(false)
+        loginViewModel = LoginViewModel(tokenRepository, standardTestDispatcherRule.testDispatcher)
+        whenever(tokenRepository.authorizeOAuth(code)).thenReturn(Result.failure(CommonException.NetworkError()))
+
+        loginViewModel.loginWithGitHub(code)
+        advanceUntilIdle()
+
+        val uiState = loginViewModel.uiState.first()
+        assertTrue(uiState is LoginViewModel.UiState.Error)
+        assertEquals((uiState as LoginViewModel.UiState.Error).errorMessage, CONNECTION_FAIL)
     }
 }
