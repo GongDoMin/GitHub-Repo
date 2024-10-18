d4ed04d23e2f5792f7892036e07727c966b37481
GongDoMin <yhw198@naver.com>
2024-10-19 00:21:40 +0900

    feat: FakeRepository 및 FakeTokenReposiory 구현 및 의존성 주입

--- data/src/main/java/com/prac/data/fake/repository/FakeTokenRepository.kt
+++ data/src/main/java/com/prac/data/fake/repository/FakeTokenRepository.kt
@@ -3,34 +3,37 @@ package com.prac.data.fake.repository
 import com.prac.data.exception.CommonException
 import com.prac.data.repository.TokenRepository
 import com.prac.data.source.local.datastore.TokenLocalDto
-import java.io.IOException
 import java.time.Instant
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class FakeTokenRepository: TokenRepository {
 
-    private lateinit var throwable: Throwable
-
     private var token: TokenLocalDto = makeEmptyToken()
-
-    fun setThrowable(throwable: Throwable) {
-        this.throwable = throwable
-    }
 
     fun setInitialToken() {
         token = makeNewToken()
     }
 
+    /**
+     * @param code 요청 코드.
+     *
+     * | 값 | 설명 |
+     * |---|---|
+     * | `success` | 성공 |
+     * | `ioException` | IOException 발생 |
+     * | `else` | 기타 오류 |
+     */
+
     override suspend fun authorizeOAuth(code: String): Result<Unit> {
-        if (::throwable.isInitialized) {
-            return when(throwable) {
-                is IOException -> Result.failure(CommonException.NetworkError())
-                else -> Result.failure(CommonException.AuthorizationError())
-            }
+        if (code == "success") {
+            return Result.success(Unit)
         }
 
-        return Result.success(Unit)
+        return when (code) {
+            "ioException" -> Result.failure(CommonException.NetworkError())
+            else -> Result.failure(CommonException.AuthorizationError())
+        }
     }
 
     override suspend fun isLoggedIn(): Boolean {
