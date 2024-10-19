11bef8f5a7d241bb9e2a7db93814e2c17d2e2cff
GongDoMin <yhw198@naver.com>
2024-10-20 00:30:33 +0900

    refact: UserLocalDataSource 구현체 cachedUserName 변수 생성

--- data/src/main/java/com/prac/data/source/local/impl/UserLocalDataSourceImpl.kt
+++ data/src/main/java/com/prac/data/source/local/impl/UserLocalDataSourceImpl.kt
@@ -2,20 +2,32 @@ package com.prac.data.source.local.impl
 
 import com.prac.data.source.local.UserLocalDataSource
 import com.prac.data.source.local.datastore.user.UserDataStoreManager
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 internal class UserLocalDataSourceImpl @Inject constructor(
     private val userDataStoreManager: UserDataStoreManager
 ) : UserLocalDataSource {
+
+    private var cachedUserName = ""
+
+    init {
+        runBlocking {
+            cachedUserName = userDataStoreManager.getUserName()
+        }
+    }
+
     override suspend fun setUserName(userName: String) {
         userDataStoreManager.setUserName(userName)
+        cachedUserName = userName
     }
 
     override suspend fun getUserName(): String {
-        return userDataStoreManager.getUserName()
+        return cachedUserName
     }
 
     override suspend fun clearUserName() {
         userDataStoreManager.clearUserName()
+        cachedUserName = ""
     }
 }
