c97533f9365a19f0cd1fc17db4f730fb0de5079e
GongDoMin <yhw198@naver.com>
2024-10-20 16:24:23 +0900

    refact: FakeGitHubService 로직 변경 및 RepoApiDataSourceTest 메서드명, 로직 변경

--- network/src/test/java/com/prac/network/RepoApiDataSourceTest.kt
+++ network/src/test/java/com/prac/network/RepoApiDataSourceTest.kt
@@ -12,10 +12,10 @@ import org.junit.Test
 
 class RepoApiDataSourceTest {
 
-    private lateinit var gitHubService: GitHubService
+    private lateinit var gitHubService: FakeGitHubService
     private lateinit var repoApiDatasource: RepoApiDataSource
 
-    private val repoList = listOf(
+    private val expectedRepoList = listOf(
         RepoDto(0, "test1", OwnerDto("test1", "test1"), 0, "test1"),
         RepoDto(1, "test2", OwnerDto("test2", "test2"), 0, "test1"),
         RepoDto(2, "test3", OwnerDto("test3", "test3"), 0, "test1"),
@@ -24,38 +24,39 @@ class RepoApiDataSourceTest {
 
     @Before
     fun setUp() {
-        gitHubService = FakeGitHubService(repoList)
+        gitHubService = FakeGitHubService()
         repoApiDatasource = RepoApiDataSourceImpl(gitHubService)
     }
 
     @Test
-    fun getRepositories_repositoriesPassedToDataSource() = runTest {
-        val repoName = "test"
+    fun getRepositories_whenCalled_repoDtoList() = runTest {
+        val userName = "test"
         val perPage = 10
         val page = 1
 
-        val result = repoApiDatasource.getRepositories(repoName, perPage, page)
+        val result = repoApiDatasource.getRepositories(userName, perPage, page)
 
+        assertEquals(result.size, expectedRepoList.size)
         result.indices.forEach {
-            assertEquals(result[it].id, repoList[it].id)
-            assertEquals(result[it].name, repoList[it].name)
-            assertEquals(result[it].owner.login, repoList[it].owner.login)
-            assertEquals(result[it].owner.avatarUrl, repoList[it].owner.avatarUrl)
-            assertEquals(result[it].stargazersCount, repoList[it].stargazersCount)
-            assertEquals(result[it].updatedAt, repoList[it].updatedAt)
+            assertEquals(result[it].id, expectedRepoList[it].id)
+            assertEquals(result[it].name, expectedRepoList[it].name)
+            assertEquals(result[it].owner.login, expectedRepoList[it].owner.login)
+            assertEquals(result[it].owner.avatarUrl, expectedRepoList[it].owner.avatarUrl)
+            assertEquals(result[it].stargazersCount, expectedRepoList[it].stargazersCount)
+            assertEquals(result[it].updatedAt, expectedRepoList[it].updatedAt)
         }
     }
 
     @Test
-    fun getRepository_repositoryPassedToDataSource() = runTest {
-        val repo = repoList[0]
+    fun getRepository_whenCalled_repoDetailDto() = runTest {
+        val expectedRepo = expectedRepoList[0]
 
-        val result = repoApiDatasource.getRepository(repo.owner.login, repo.name)
+        val result = repoApiDatasource.getRepository(expectedRepo.owner.login, expectedRepo.name)
 
-        assertEquals(result.id, repo.id)
-        assertEquals(result.name, repo.name)
-        assertEquals(result.owner.login, repo.owner.login)
-        assertEquals(result.owner.avatarUrl, repo.owner.avatarUrl)
-        assertEquals(result.stargazersCount, repo.stargazersCount)
+        assertEquals(result.id, expectedRepo.id)
+        assertEquals(result.name, expectedRepo.name)
+        assertEquals(result.owner.login, expectedRepo.owner.login)
+        assertEquals(result.owner.avatarUrl, expectedRepo.owner.avatarUrl)
+        assertEquals(result.stargazersCount, expectedRepo.stargazersCount)
     }
 }
