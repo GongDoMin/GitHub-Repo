238ba3954909302ad79c1342a541fc665dc21366
GongDoMin <yhw198@naver.com>
2024-10-21 23:38:25 +0900

    refact: FakeGitHubAuthService 로직 변경 및 AuthApiDataSourceTest 메서드명, 로직 변경

--- network/src/main/java/com/prac/network/fake/service/FakeGitHubAuthService.kt
+++ network/src/main/java/com/prac/network/fake/service/FakeGitHubAuthService.kt
@@ -3,11 +3,16 @@ package com.prac.network.fake.service
 import com.prac.network.dto.TokenDto
 import com.prac.network.service.GitHubAuthService
 
-internal class FakeGitHubAuthService(
-    private val token: TokenDto
-) : GitHubAuthService {
+internal class FakeGitHubAuthService: GitHubAuthService {
     override suspend fun authorizeOAuth(accept: String, clientID: String, clientSecret: String, code: String): TokenDto {
-        return token
+        return TokenDto(
+            accessToken = "accessToken",
+            expiresIn = 3600,
+            refreshToken= "refreshToken",
+            refreshTokenExpiresIn = 18000,
+            scope = "",
+            tokenType = "Bearer"
+        )
     }
 
     override suspend fun refreshAccessToken(accept: String, clientID: String, clientSecret: String, grantType: String, refreshToken: String): TokenDto {
--- network/src/test/java/com/prac/network/AuthApiDataSourceTest.kt
+++ network/src/test/java/com/prac/network/AuthApiDataSourceTest.kt
@@ -12,35 +12,52 @@ import org.junit.Test
 
 internal class AuthApiDataSourceTest {
 
-    private lateinit var gitHubAuthService: GitHubAuthService
+    private lateinit var gitHubAuthService: FakeGitHubAuthService
     private lateinit var authApiDataSource: AuthApiDataSource
 
-    private val token = TokenDto("accessToken", 3600, "refreshToken", 3600, "", "Bearer")
     private val code = "code"
 
     @Before
     fun setup() {
-        gitHubAuthService = FakeGitHubAuthService(token)
+        gitHubAuthService = FakeGitHubAuthService()
         authApiDataSource = AuthApiDataSourceImpl(gitHubAuthService)
     }
 
     @Test
-    fun authorizeOAuth_tokenPassedToDataSource() = runTest {
+    fun authorizeOAuth_whenCalled_token() = runTest {
+        val expectedToken = TokenDto(
+            accessToken = "accessToken",
+            expiresIn = 3600,
+            refreshToken= "refreshToken",
+            refreshTokenExpiresIn = 18000,
+            scope = "",
+            tokenType = "Bearer"
+        )
 
         val result = authApiDataSource.authorizeOAuth(code)
 
-        assertEquals(result.accessToken, token.accessToken)
-        assertEquals(result.refreshToken, token.refreshToken)
-        assertEquals(result.expiresIn, token.expiresIn)
-        assertEquals(result.refreshTokenExpiresIn, token.refreshTokenExpiresIn)
+        assertEquals(result.accessToken, expectedToken.accessToken)
+        assertEquals(result.refreshToken, expectedToken.refreshToken)
+        assertEquals(result.expiresIn, expectedToken.expiresIn)
+        assertEquals(result.refreshTokenExpiresIn, expectedToken.refreshTokenExpiresIn)
     }
 
     @Test
-    fun refreshAccessToken_tokenPassedToDataSource() = runTest {
+    fun refreshAccessToken_whenCalled_refreshToken() = runTest {
+        val expectedToken = TokenDto(
+            accessToken = "refreshAccessToken",
+            expiresIn = 3600,
+            refreshToken= "refreshRefreshToken",
+            refreshTokenExpiresIn = 18000,
+            scope = "",
+            tokenType = "Bearer"
+        )
 
         val result = authApiDataSource.refreshAccessToken(code)
 
-        assertNotEquals(result.accessToken, token.accessToken)
-        assertNotEquals(result.refreshToken, token.refreshToken)
+        assertEquals(result.accessToken, expectedToken.accessToken)
+        assertEquals(result.refreshToken, expectedToken.refreshToken)
+        assertEquals(result.expiresIn, expectedToken.expiresIn)
+        assertEquals(result.refreshTokenExpiresIn, expectedToken.refreshTokenExpiresIn)
     }
 }
