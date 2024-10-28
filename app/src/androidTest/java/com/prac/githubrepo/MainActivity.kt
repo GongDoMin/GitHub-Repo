d0ab286877bc8ba8a50fde2f6d967cd57cb35242
GongDoMin <yhw198@naver.com>
2024-10-28 11:47:32 +0900

    feat: test 를 위한 MainActivity 추가

---
+++ app/src/androidTest/java/com/prac/githubrepo/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.prac.githubrepo
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity()
