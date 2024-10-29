4d62e2f90b55518f827f517c374c61ebf263f32a
GongDoMin <yhw198@naver.com>
2024-10-29 12:24:18 +0900

    feat: IconID SemanticsProperty 생성

--- app/src/main/java/com/prac/githubrepo/util/SemanticsPropertyKey.kt
+++ app/src/main/java/com/prac/githubrepo/util/SemanticsPropertyKey.kt
@@ -1,5 +1,6 @@
 package com.prac.githubrepo.util
 
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 
@@ -8,3 +9,6 @@ var SemanticsPropertyReceiver.drawableID by DrawableID
 
 val ButtonID = SemanticsPropertyKey<Int>("buttonID")
 var SemanticsPropertyReceiver.buttonID by ButtonID
+
+val IconID = SemanticsPropertyKey<ImageVector>("iconID")
+var SemanticsPropertyReceiver.iconID by IconID
