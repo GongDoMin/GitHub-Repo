eaa3ac3cb8f7884df4b4d6b835ed33c37545278d
GongDoMin <yhw198@naver.com>
2024-11-02 19:52:56 +0900

    refact: FakeTokenLocalDataSource 생성자 추가 및 TokenRepositoryTest 로직 수정

--- data/src/test/java/com/prac/data/repository/TokenRepositoryTest.kt
+++ data/src/test/java/com/prac/data/repository/TokenRepositoryTest.kt
@@ -14,6 +14,8 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import java.io.IOException
+import java.time.Instant
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 class TokenRepositoryTest {
@@ -25,24 +27,18 @@ class TokenRepositoryTest {
 
     private lateinit var tokenRepository: TokenRepository
 
+    private val token = TokenLocalDto(
+        accessToken = "accessToken",
+        refreshToken = "refreshToken",
+        expiresInSeconds = 3600,
+        refreshTokenExpiresInSeconds = 3600,
+        updatedAt = Instant.now().atZone(ZoneId.systemDefault())
+    )
     private val code = "code"
-
-    @Before
-    fun setUp() {
-        tokenLocalDataSource = FakeTokenLocalDataSource()
-        authApiDataSource = FakeAuthApiDataSource()
-        userApiDataSource = FakeUserApiDataSource()
-        userLocalDataSource = FakeUserLocalDataSource()
-        tokenRepository = TokenRepositoryImpl(
-            tokenLocalDataSource = tokenLocalDataSource,
-            authApiDataSource = authApiDataSource,
-            userApiDataSource = userApiDataSource,
-            userLocalDataSource = userLocalDataSource
-        )
-    }
 
     @Test
     fun authorizeOAuth_apiCallIsSuccess_successAndUpdateTokenAndUserName() = runTest {
+        makeTokenRepository()
         val expectedUserName = "test"
         val expectedToken = TokenLocalDto(
             accessToken = "accessToken",
@@ -66,6 +62,7 @@ class TokenRepositoryTest {
 
     @Test
     fun authorizeOAuth_authorizeOAuthIsFailure_networkErrorAndNotUpdateTokenAndUserName() = runTest {
+        makeTokenRepository()
         authApiDataSource.setThrowable(IOException())
 
         val result = tokenRepository.authorizeOAuth(code)
@@ -83,6 +80,7 @@ class TokenRepositoryTest {
 
     @Test
     fun authorizeOAuth_authorizeOAuthIsFailure_authorizationErrorAndNotUpdateTokenAndUserName() = runTest {
+        makeTokenRepository()
         val exception = IllegalArgumentException() // IOException 제외한 모든 에러는 authorizationError 로 처리하기 때문에 Exception 의 종류는 상관없음.
         authApiDataSource.setThrowable(exception)
 
@@ -101,6 +99,7 @@ class TokenRepositoryTest {
 
     @Test
     fun authorizeOAuth_getUserNameIsFailure_networkErrorAndNotUpdateTokenAndUserName() = runTest {
+        makeTokenRepository()
         userApiDataSource.setThrowable(IOException())
 
         val result = tokenRepository.authorizeOAuth(code)
@@ -118,6 +117,7 @@ class TokenRepositoryTest {
 
     @Test
     fun authorizeOAuth_getUserNameIsFailure_authorizationErrorAndNotUpdateTokenAndUserName() = runTest {
+        makeTokenRepository()
         val exception = IllegalArgumentException() // IOException 제외한 모든 에러는 authorizationError 로 처리하기 때문에 Exception 의 종류는 상관없음.
         authApiDataSource.setThrowable(exception)
 
@@ -136,7 +136,7 @@ class TokenRepositoryTest {
 
     @Test
     fun refreshToken_refreshTokenIsSuccess_successAndUpdateToken() = runTest {
-        tokenLocalDataSource.setInitialToken()
+        makeTokenRepositoryWithInitialToken()
         val refreshToken = "refreshToken"
         val expectedToken = TokenLocalDto(
             accessToken = "refreshAccessToken",
@@ -158,7 +158,7 @@ class TokenRepositoryTest {
 
     @Test
     fun refreshToken_refreshTokenIsFailure_errorAndNotUpdateToken() = runTest {
-        tokenLocalDataSource.setInitialToken()
+        makeTokenRepositoryWithInitialToken()
         val refreshToken = "refreshToken"
         val expectedToken = TokenLocalDto(
             accessToken = "accessToken",
@@ -181,7 +181,7 @@ class TokenRepositoryTest {
 
     @Test
     fun isLoggedIn_tokenIsExist_true() = runTest {
-        tokenLocalDataSource.setInitialToken()
+        makeTokenRepositoryWithInitialToken()
 
         val isLoggedIn = tokenRepository.isLoggedIn()
 
@@ -190,6 +190,7 @@ class TokenRepositoryTest {
 
     @Test
     fun isLoggedIn_tokenIsNotExist_false() = runTest {
+        makeTokenRepository()
 
         val isLoggedIn = tokenRepository.isLoggedIn()
 
@@ -198,7 +199,7 @@ class TokenRepositoryTest {
 
     @Test
     fun clearToken_clearTokenAndUserName_tokenAndUserNameIsEmpty() = runTest {
-        tokenLocalDataSource.setInitialToken()
+        makeTokenRepositoryWithInitialToken()
         userLocalDataSource.setInitialUserName()
 
         tokenRepository.clearToken()
@@ -208,5 +209,31 @@ class TokenRepositoryTest {
         assertTrue(token.accessToken.isEmpty())
         assertTrue(token.refreshToken.isEmpty())
         assertTrue(userName.isEmpty())
+    }
+
+    private fun makeTokenRepository() {
+        tokenLocalDataSource = FakeTokenLocalDataSource()
+        authApiDataSource = FakeAuthApiDataSource()
+        userApiDataSource = FakeUserApiDataSource()
+        userLocalDataSource = FakeUserLocalDataSource()
+        tokenRepository = TokenRepositoryImpl(
+            tokenLocalDataSource = tokenLocalDataSource,
+            authApiDataSource = authApiDataSource,
+            userApiDataSource = userApiDataSource,
+            userLocalDataSource = userLocalDataSource
+        )
+    }
+
+    private fun makeTokenRepositoryWithInitialToken() {
+        tokenLocalDataSource = FakeTokenLocalDataSource(token)
+        authApiDataSource = FakeAuthApiDataSource()
+        userApiDataSource = FakeUserApiDataSource()
+        userLocalDataSource = FakeUserLocalDataSource()
+        tokenRepository = TokenRepositoryImpl(
+            tokenLocalDataSource = tokenLocalDataSource,
+            authApiDataSource = authApiDataSource,
+            userApiDataSource = userApiDataSource,
+            userLocalDataSource = userLocalDataSource
+        )
     }
 }
