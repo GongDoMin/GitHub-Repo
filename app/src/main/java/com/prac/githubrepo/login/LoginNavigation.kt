bdf91210f7270b4ed98128a5c5eab9760b8270f0
GongDoMin <yhw198@naver.com>
2024-11-08 00:34:56 +0900

    feat: LoginNavigation 추가

---
+++ app/src/main/java/com/prac/githubrepo/login/LoginNavigation.kt
@@ -0,0 +1,26 @@
+package com.prac.githubrepo.login
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+
+const val LOGIN_SCREEN = "login"
+
+fun NavController.navigationToLogin(
+    navOptions: NavOptions
+) {
+    navigate(LOGIN_SCREEN, navOptions)
+}
+
+fun NavGraphBuilder.loginScreen(
+    onLogin: () -> Unit
+) {
+    composable(
+        route = LOGIN_SCREEN
+    ) {
+        LoginScreen(
+            onLogin = onLogin
+        )
+    }
+}
