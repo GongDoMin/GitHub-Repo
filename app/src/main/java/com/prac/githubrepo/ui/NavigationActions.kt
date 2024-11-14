ecee6410d3d57159c42691806d337271dcad6f04
GongDoMin <yhw198@naver.com>
2024-11-14 23:24:05 +0900

    feat: NavigationActions 메서드 추가

--- app/src/main/java/com/prac/githubrepo/ui/NavigationActions.kt
+++ app/src/main/java/com/prac/githubrepo/ui/NavigationActions.kt
@@ -1,7 +1,54 @@
 package com.prac.githubrepo.ui
 
 import androidx.navigation.NavController
+import com.prac.githubrepo.ui.Routes.HOME
+import com.prac.githubrepo.ui.Routes.LOGIN
+import com.prac.githubrepo.ui.Routes.PROFILE
 
 class NavigationActions(private val navController: NavController) {
+    fun popBackStack() {
+        navController.popBackStack()
+    }
 
+    fun navigateToLogin() {
+        navController.navigate(LOGIN) {
+            popUpTo(navController.graph.id) {
+                inclusive = true
+            }
+        }
+    }
+
+    fun navigateToBottom(route: Routes) {
+        when (route) {
+            HOME -> {
+                navController.navigate(route) {
+                    popUpTo(navController.graph.id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+            PROFILE -> {
+                navController.navigate(route) {
+                    popUpTo(navController.graph.id) {
+                        saveState = true
+                    }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+            else -> throw IllegalArgumentException("Invalid route")
+        }
+    }
+
+    fun navigateToMain() {
+        navController.navigate(HOME.MAIN) {
+            popBackStack()
+        }
+    }
+
+    fun navigateToDetail(userName: String, repoName: String) {
+        navController.navigate(HOME.DETAIL(userName, repoName))
+    }
 }
