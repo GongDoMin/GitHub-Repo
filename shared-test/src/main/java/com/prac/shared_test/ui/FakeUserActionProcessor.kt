f0d661eba224aa42f15515bde20bcd177ed8f5b7
GongDoMin <yhw198@naver.com>
2024-11-20 23:39:58 +0900

    feat: FakeLoginActionProcess, FakeUserActionProcessor 추가

---
+++ shared-test/src/main/java/com/prac/shared_test/ui/FakeUserActionProcessor.kt
@@ -0,0 +1,20 @@
+package com.prac.shared_test.ui
+
+import com.prac.githubrepo.common.ActionProcessor
+import com.prac.githubrepo.ui.login.model.Action
+import com.prac.githubrepo.ui.login.model.Event
+import com.prac.githubrepo.ui.login.model.UiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeUserActionProcessor : ActionProcessor<Action, UiState, Event> {
+    override fun invoke(action: Action): Flow<Pair<UiState?, Event?>> =
+        flow {
+            println("?????")
+            when (action) {
+                is Action.OnClickLoginButton -> emit(null to Event.LaunchLoginIntent)
+                is Action.DialogDismiss -> emit(UiState.Idle to null)
+                else -> Unit
+            }
+        }
+}
