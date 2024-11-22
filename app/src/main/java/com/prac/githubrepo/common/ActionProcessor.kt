d53d83627d9ed3c0e89a4bded9eac1d292fa47b9
GongDoMin <yhw198@naver.com>
2024-11-22 15:32:41 +0900

    refact: 사용하지 않는 클래스 제거

--- app/src/main/java/com/prac/githubrepo/common/ActionProcessor.kt
+++
@@ -1,7 +0,0 @@
-package com.prac.githubrepo.common
-
-import kotlinx.coroutines.flow.Flow
-
-interface ActionProcessor<Action, UiState, Event> {
-    operator fun invoke(action: Action): Flow<Pair<UiState?, Event?>>
-}
