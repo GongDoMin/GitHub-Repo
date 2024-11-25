461f77e507435ef9367c03681d2b77860bbb9aff
GongDoMin <yhw198@naver.com>
2024-11-25 15:43:58 +0900

    feat: builde-logic, convention 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
