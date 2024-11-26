fd264e144e2a0d6e43867f717f5d5c4450b0df09
GongDoMin <yhw198@naver.com>
2024-11-26 20:42:30 +0900

    refact: data gradle plugin 으로 변경

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -6,28 +6,15 @@ val localProperties = Properties().apply {
 }
 
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.jetbrains.kotlin.kapt)
-    alias(libs.plugins.dagger.hilt)
+    id("githubrepo.android.library")
+    id("githubrepo.android.room")
 }
 
 android {
     namespace = "com.prac.data"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
 
     buildTypes {
         debug {
-            isMinifyEnabled = false
-
             buildConfigField("String", "GITHUB_URL", "\"https://github.com/\"")
             buildConfigField("String", "GITHUB_API_URL", "\"https://api.github.com/\"")
             buildConfigField("String", "CLIENT_ID", "" + localProperties["CLIENT_ID"] + "")
@@ -35,53 +22,20 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-
             buildConfigField("String", "GITHUB_URL", "\"https://github.com/\"")
+            buildConfigField("String", "GITHUB_API_URL", "\"https://api.github.com/\"")
+            buildConfigField("String", "CLIENT_ID", "" + localProperties["CLIENT_ID"] + "")
+            buildConfigField("String", "CLIENT_SECRET", "" + localProperties["CLIENT_SECRET"] + "")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        buildConfig = true
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    testImplementation(libs.junit)
-    testImplementation(libs.androidx.junit)
-
     implementation(project(":local"))
     implementation(project(":network"))
     testImplementation(project(":shared-test"))
-
-    implementation(libs.jetbrains.kotlinx.coroutines)
-    implementation(libs.jetbrains.kotlinx.coroutines.test)
-
-    implementation(libs.dagger.hilt.android)
-    implementation(libs.hilt.android.testing)
     androidTestImplementation(project(":shared-test"))
-    kapt(libs.dagger.hilt.compiler)
 
     implementation(libs.retrofit)
-
-    implementation(libs.androidx.paging)
-
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.paging)
-    kapt(libs.androidx.room.compiler)
-    androidTestImplementation(libs.androidx.room.testing)
 }
