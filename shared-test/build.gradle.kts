ca46c7a0f3fb535fa6514108290628d3e1de2238
GongDoMin <yhw198@naver.com>
2024-11-26 20:41:15 +0900

    refact: shared-test gradle plugin 으로 변경

--- shared-test/build.gradle.kts
+++ shared-test/build.gradle.kts
@@ -1,60 +1,18 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.jetbrains.kotlin.kapt)
+    id("githubrepo.android.library")
+    id("githubrepo.android.room")
 }
 
 android {
     namespace = "com.prac.shared_test"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 28
-
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
 
     implementation(project(":app"))
     implementation(project(":data"))
     implementation(project(":local"))
     implementation(project(":network"))
 
-    implementation(libs.jetbrains.kotlinx.coroutines)
-    implementation(libs.jetbrains.kotlinx.coroutines.test)
-
-    implementation(libs.dagger.hilt.android)
-    implementation(libs.hilt.android.testing)
-    kapt(libs.dagger.hilt.compiler)
-
     implementation(libs.retrofit)
-
-    implementation(libs.androidx.paging)
-
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.paging)
-    kapt(libs.androidx.room.compiler)
 }
