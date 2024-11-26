f4e889a7d41ed03f21fe5c3a34d954705349d553
GongDoMin <yhw198@naver.com>
2024-11-26 20:42:39 +0900

    refact: app gradle plugin 으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import kotlinx.coroutines.flow.firstOrNull
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -7,26 +6,21 @@ val localProperties = Properties().apply {
 }
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.jetbrains.kotlin.kapt)
-    alias(libs.plugins.dagger.hilt)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("githubrepo.android.application")
+    id("githubrepo.android.application.compose")
+    id("githubrepo.android.hilt")
+    id("githubrepo.android.coroutines")
+    id("githubrepo.android.mockito")
+    id("githubrepo.android.serialization")
 }
 
 android {
     namespace = "com.prac.githubrepo"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.prac.githubrepo"
-        minSdk = 28
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "com.prac.githubrepo.CustomTestRunner"
     }
 
     buildTypes {
@@ -42,26 +36,14 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            buildConfigField("String", "GITHUB_OAUTH_URI", "" + localProperties["GITHUB_OAUTH_URI"] + "")
         }
     }
     sourceSets {
         getByName("debug") {
             java.srcDirs("src/debug/java")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    viewBinding {
-        enable = true
-    }
-    buildFeatures {
-        buildConfig = true
-        compose = true
     }
 }
 
@@ -73,15 +55,14 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.ui.test.junit4.android)
     implementation(libs.androidx.navigation.testing)
+    implementation(libs.androidx.junit.ktx)
     testImplementation(libs.junit)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.kotlin)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.runner)
     androidTestImplementation(libs.androidx.rules)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.espresso.intents)
     androidTestImplementation(libs.androidx.espresso.contrib)
     androidTestImplementation(libs.hamcrest)
@@ -90,35 +71,12 @@ dependencies {
     testImplementation(project(":shared-test"))
     androidTestImplementation(project(":shared-test"))
 
-    implementation(libs.jetbrains.kotlinx.coroutines)
-    implementation(libs.jetbrains.kotlinx.coroutines.test)
-
-    implementation(libs.dagger.hilt.android)
-    androidTestImplementation(libs.hilt.android.testing)
-    testImplementation(libs.hilt.android.testing)
     kaptTest(libs.hilt.android.compiler)
     kaptAndroidTest(libs.hilt.android.compiler)
-    kapt(libs.dagger.hilt.compiler)
 
     implementation(libs.androidx.lifecycle.viewmodel)
     implementation(libs.androidx.lifecycle.runtime)
 
-    implementation(libs.androidx.paging)
-
-    implementation(platform(libs.compose.bom))
-    androidTestImplementation(platform(libs.compose.bom))
-
-    implementation(libs.androidx.runtime)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.foundation)
-    implementation(libs.androidx.foundation.layout)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.runtime.livedata)
-    implementation(libs.androidx.ui.tooling)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.paging.compose)
     implementation(libs.compose.glide)
-
-    implementation(libs.jetbrains.kotlinx.serialization)
 }
