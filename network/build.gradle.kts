b1e1cd7862f047230b66eddce69402f3497c9c9c
GongDoMin <yhw198@naver.com>
2024-11-26 20:41:42 +0900

    refact: network gradle plugin 으로 변경

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -6,28 +6,16 @@ val localProperties = Properties().apply {
 }
 
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.jetbrains.kotlin.kapt)
-    alias(libs.plugins.dagger.hilt)
-    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("githubrepo.android.library")
+    id("githubrepo.android.serialization")
+    id("githubrepo.android.mockito")
 }
 
 android {
     namespace = "com.prac.network"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
 
     buildTypes {
         debug {
-            isMinifyEnabled = false
-
             buildConfigField("String", "GITHUB_URL", "\"https://github.com/\"")
             buildConfigField("String", "GITHUB_API_URL", "\"https://api.github.com/\"")
             buildConfigField("String", "CLIENT_ID", "" + localProperties["CLIENT_ID"] + "")
@@ -35,50 +23,20 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            buildConfigField("String", "GITHUB_URL", "\"https://github.com/\"")
+            buildConfigField("String", "GITHUB_API_URL", "\"https://api.github.com/\"")
+            buildConfigField("String", "CLIENT_ID", "" + localProperties["CLIENT_ID"] + "")
+            buildConfigField("String", "CLIENT_SECRET", "" + localProperties["CLIENT_SECRET"] + "")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        buildConfig = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.junit.ktx)
-    testImplementation(libs.junit)
-    testImplementation(libs.androidx.junit)
-    testImplementation(libs.mockito.core)
-    testImplementation(libs.mockito.kotlin)
-
     implementation(project(":local"))
     testImplementation(project(":shared-test"))
 
-    implementation(libs.jetbrains.kotlinx.coroutines)
-    implementation(libs.jetbrains.kotlinx.coroutines.test)
-
-    implementation(libs.dagger.hilt.android)
-    implementation(libs.hilt.android.testing)
-    kapt(libs.dagger.hilt.compiler)
-
     implementation(libs.okhttp)
-    implementation(libs.logging.interceptor)
-
     implementation(libs.retrofit)
+    implementation(libs.logging.interceptor)
     implementation(libs.retrofit.serialization.converter)
-
-    implementation(libs.jetbrains.kotlinx.serialization)
 }
