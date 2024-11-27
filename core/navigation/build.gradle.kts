208f4bb06b390a24d37d45ba9cc763e4de53e1e8
GongDoMin <yhw198@naver.com>
2024-11-27 20:09:18 +0900

    feat: core:navigation module 추가

---
+++ core/navigation/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("githubrepo.android.library")
+    id("githubrepo.android.library.compose")
+    id("githubrepo.android.serialization")
+}
+
+
+android {
+    namespace = "com.prac.core.navigation"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
