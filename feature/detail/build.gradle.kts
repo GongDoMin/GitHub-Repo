dc1634ceffb211b7ecc70ce3d902974379c9f661
GongDoMin <yhw198@naver.com>
2024-11-27 16:53:55 +0900

    feat: feature:detail module 생성

---
+++ feature/detail/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("githubrepo.android.library")
+    id("githubrepo.android.library.compose")
+    id("githubrepo.android.mockito")
+}
+
+android {
+    namespace = "com.prac.feature.detail"
+}
+
+dependencies {
+
+    implementation(project(":core:common"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:data"))
+    implementation(project(":core:exception"))
+    testImplementation(project(":shared-test"))
+    androidTestImplementation(project(":shared-test"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    testImplementation(libs.turbine)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime)
+}
