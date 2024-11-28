deb75510fcba6a73af015dcfa3eb2db5f96d445c
GongDoMin <yhw198@naver.com>
2024-11-28 15:56:44 +0900

    feat: feature:home module 추가

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("githubrepo.android.library")
+    id("githubrepo.android.library.compose")
+}
+
+android {
+    namespace = "com.prac.feature.home"
+}
+
+dependencies {
+
+    implementation(project(":feature:main"))
+    implementation(project(":feature:detail"))
+
+    implementation(project(":core:common"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:data"))
+    implementation(project(":core:exception"))
+    implementation(project(":core:navigation"))
+    testImplementation(project(":shared-test"))
+    androidTestImplementation(project(":shared-test"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
