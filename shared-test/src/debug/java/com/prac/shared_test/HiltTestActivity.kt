aa785dc19439422ba8945df85fde7503bde69234
GongDoMin <yhw198@naver.com>
2024-11-29 22:12:45 +0900

    refact: shared-test module sourceSets debug 추가 및 dependencies 변경

---
+++ shared-test/src/debug/java/com/prac/shared_test/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.prac.shared_test
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
