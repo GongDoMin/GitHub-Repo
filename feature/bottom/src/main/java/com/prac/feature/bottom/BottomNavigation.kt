22bd7472adac4b2adcc4aa2e68b9acc0d88df503
GongDoMin <yhw198@naver.com>
2024-11-30 00:42:57 +0900

    feat: BottomNavigation Preview 추가

--- feature/bottom/src/main/java/com/prac/feature/bottom/BottomNavigation.kt
+++ feature/bottom/src/main/java/com/prac/feature/bottom/BottomNavigation.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.prac.core.designsystem.R
 import com.prac.core.navigation.Routes
@@ -61,4 +62,24 @@ fun GitHubBottomNavigation(
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun GitHubBottomNavigationHomePreview() {
+    GitHubBottomNavigation(
+        onNavigationToBottom = {},
+        bottomNavItemList = listOf(BottomNavItem.Home, BottomNavItem.Profile),
+        currentBottomNavItem = BottomNavItem.Home
+    )
+}
+
+@Preview
+@Composable
+fun GitHubBottomNavigationProfilePreview() {
+    GitHubBottomNavigation(
+        onNavigationToBottom = {},
+        bottomNavItemList = listOf(BottomNavItem.Home, BottomNavItem.Profile),
+        currentBottomNavItem = BottomNavItem.Profile
+    )
 }
