1702cfd6a88e6aac7c892fc4acedcc19b7fcd789
GongDoMin <yhw198@naver.com>
2024-11-29 22:16:19 +0900

    refact: profileScreenTest feature:profile module 로 이동

--- feature/profile/src/androidTest/java/com/prac/feature/profile/ProfileScreenTest.kt
+++ feature/profile/src/androidTest/java/com/prac/feature/profile/ProfileScreenTest.kt
@@ -1,12 +1,12 @@
-package com.prac.githubrepo.profile
+package com.prac.feature.profile
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.isNotDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import com.prac.githubrepo.HiltTestActivity
 import com.prac.core.designsystem.R
+import com.prac.shared_test.HiltTestActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.test.runTest
