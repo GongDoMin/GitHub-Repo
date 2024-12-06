56fb18bd01f17e3f80ac1478bc2f0bcee8347866
GongDoMin <yhw198@naver.com>
2024-12-06 19:05:04 +0900

    refact: UserProfile package 변경

--- core/common/src/main/java/com/prac/core/common/ui/UserProfile.kt
+++ core/common/src/main/java/com/prac/core/common/ui/UserProfile.kt
@@ -1,4 +1,4 @@
-package com.prac.core.designsystem.component
+package com.prac.core.common.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- feature/detail/src/main/java/com/prac/feature/detail/view/DetailContent.kt
+++ feature/detail/src/main/java/com/prac/feature/detail/view/DetailContent.kt
@@ -39,7 +39,7 @@ import com.prac.core.common.constants.INVALID_REPOSITORY
 import com.prac.core.designsystem.R
 import com.prac.core.common.ui.MessageDialog
 import com.prac.core.designsystem.component.ContentWithLoadingIndicator
-import com.prac.core.designsystem.component.UserProfile
+import com.prac.core.common.ui.UserProfile
 import com.prac.data.model.RepoDetailModel
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
