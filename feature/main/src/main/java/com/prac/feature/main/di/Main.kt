2cf6c4b45fb46eac3176293cc588974edd28e1a3
GongDoMin <yhw198@naver.com>
2024-12-08 23:15:03 +0900

    refact: Main 으로 이름 변경 및 MainActionProcessor hilt 주입

---
+++ feature/main/src/main/java/com/prac/feature/main/di/Main.kt
@@ -0,0 +1,50 @@
+package com.prac.feature.main.di
+
+import com.prac.core.common.backoff.BackOffWorkManager
+import com.prac.core.common.mvi.action.ActionProcessor
+import com.prac.core.common.mvi.reducer.Reducer
+import com.prac.data.repository.RepoRepository
+import com.prac.data.repository.TokenRepository
+import com.prac.feature.main.MainActionProcessor
+import com.prac.feature.main.MainReducerProcessor
+import com.prac.feature.main.model.Action
+import com.prac.feature.main.model.Event
+import com.prac.feature.main.model.Mutation
+import com.prac.feature.main.view.UiState
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainReducerAnnotation
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainActionAnnotation
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class MainModule {
+    @Provides
+    @MainReducerAnnotation
+    fun providesMainReducerProcessor(): Reducer<Mutation, UiState> {
+        return MainReducerProcessor()
+    }
+
+    @Provides
+    @MainActionAnnotation
+    fun providesMainActionProcessor(
+        tokenRepository: TokenRepository,
+        repoRepository: RepoRepository,
+        backOffWorkManager: BackOffWorkManager
+    ): ActionProcessor<Action, Mutation, Event> {
+        return MainActionProcessor(
+            tokenRepository = tokenRepository,
+            repoRepository = repoRepository,
+            backOffWorkManager = backOffWorkManager
+        )
+    }
+}
