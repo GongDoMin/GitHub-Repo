e400b55f2de5eed67992c8c409afc93f966aa741
GongDoMin <yhw198@naver.com>
2024-12-08 21:59:53 +0900

    refact: Login 으로 이름 변경 및 LoginActionProcessor hilt 추가

---
+++ feature/login/src/main/java/com/prac/feature/login/di/Login.kt
@@ -0,0 +1,40 @@
+package com.prac.feature.login.di
+
+import com.prac.core.common.mvi.action.ActionProcessor
+import com.prac.core.common.mvi.reducer.Reducer
+import com.prac.data.repository.TokenRepository
+import com.prac.feature.login.LoginActionProcessor
+import com.prac.feature.login.LoginReducerProcessor
+import com.prac.feature.login.model.Action
+import com.prac.feature.login.model.Event
+import com.prac.feature.login.model.Mutation
+import com.prac.feature.login.view.UiState
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoginReducerAnnotation
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoginActionAnnotation
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class LoginModule {
+    @Provides
+    @LoginReducerAnnotation
+    fun providesLoginReducerProcessor(): Reducer<Mutation, UiState> {
+        return LoginReducerProcessor()
+    }
+
+    @Provides
+    @LoginActionAnnotation
+    fun providesLoginActionProcessor(tokenRepository: TokenRepository): ActionProcessor<Action, Mutation, Event> {
+        return LoginActionProcessor(tokenRepository)
+    }
+}
