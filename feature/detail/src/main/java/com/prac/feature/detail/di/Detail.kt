e23a3bd075001614a98c30294499203903faf48b
GongDoMin <yhw198@naver.com>
2024-12-09 09:42:37 +0900

    refact: Detail 로 이름 변경 및 ActionProcessor hilt 주입

---
+++ feature/detail/src/main/java/com/prac/feature/detail/di/Detail.kt
@@ -0,0 +1,50 @@
+package com.prac.feature.detail.di
+
+import com.prac.core.common.backoff.BackOffWorkManager
+import com.prac.core.common.mvi.action.ActionProcessor
+import com.prac.core.common.mvi.reducer.Reducer
+import com.prac.data.repository.RepoRepository
+import com.prac.data.repository.TokenRepository
+import com.prac.feature.detail.DetailActionProcessor
+import com.prac.feature.detail.DetailReducerProcessor
+import com.prac.feature.detail.model.Action
+import com.prac.feature.detail.model.Event
+import com.prac.feature.detail.model.Mutation
+import com.prac.feature.detail.view.UiState
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DetailReducerAnnotation
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DetailActionAnnotation
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+class DetailModule {
+    @Provides
+    @DetailReducerAnnotation
+    fun providesDetailReducerProcessor(): Reducer<Mutation, UiState> {
+        return DetailReducerProcessor()
+    }
+
+    @Provides
+    @DetailActionAnnotation
+    fun providesDetailActionProcessor(
+        tokenRepository: TokenRepository,
+        repoRepository: RepoRepository,
+        backOffWorkManager: BackOffWorkManager
+    ): ActionProcessor<Action, Mutation, Event> {
+        return DetailActionProcessor(
+            repoRepository = repoRepository,
+            tokenRepository = tokenRepository,
+            backOffWorkManager = backOffWorkManager
+        )
+    }
+}
