8be3629a62489ac4c9a65e7561603bbf695e6374
GongDoMin <yhw198@naver.com>
2024-12-09 10:20:06 +0900

    feat: ProfileActionProcessor 추가

---
+++ feature/profile/src/main/java/com/prac/feature/profile/ProfileActionProcessor.kt
@@ -0,0 +1,49 @@
+package com.prac.feature.profile
+
+import com.prac.core.common.backoff.BackOffWorkManager
+import com.prac.core.common.mvi.action.ActionProcessor
+import com.prac.data.repository.RepoRepository
+import com.prac.data.repository.TokenRepository
+import com.prac.feature.profile.model.Action
+import com.prac.feature.profile.model.Event
+import com.prac.feature.profile.model.Mutation
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.flow
+
+class ProfileActionProcessor(
+    private val tokenRepository: TokenRepository,
+    private val repoRepository: RepoRepository,
+    private val backOffWorkManager: BackOffWorkManager
+) : ActionProcessor<Action, Mutation, Event> {
+    override fun invoke(action: Action): Flow<Pair<Mutation?, Event?>> =
+        flow {
+            when (action) {
+                is Action.UserAction.OnClickLogoutButton -> handleClickLogoutButton()
+                is Action.UserAction.DialogDismiss -> handleDialogDismiss()
+                is Action.UserAction.OnClickNegativeButton -> handleClickNegativeButton()
+                is Action.UserAction.OnClickPositiveButton -> handleClickCheckButton()
+            }
+        }
+    private suspend fun FlowCollector<Pair<Mutation?, Event?>>.handleClickLogoutButton() {
+        emit(Mutation.ShowDialog to null)
+    }
+
+    private suspend fun FlowCollector<Pair<Mutation?, Event?>>.handleDialogDismiss() {
+        emit(Mutation.ShowIdle to null)
+    }
+
+    private suspend fun FlowCollector<Pair<Mutation?, Event?>>.handleClickNegativeButton() {
+        emit(Mutation.ShowIdle to null)
+    }
+
+    private suspend fun FlowCollector<Pair<Mutation?, Event?>>.handleClickCheckButton() {
+        emit(Mutation.ShowLoading to null)
+
+        tokenRepository.clearToken()
+        backOffWorkManager.clearWork()
+        repoRepository.clearRepositories()
+
+        emit(null to Event.Logout)
+    }
+}
