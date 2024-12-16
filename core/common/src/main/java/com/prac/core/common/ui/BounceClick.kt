96377a73c2ce7bea74b511fd37567f6ca4299729
GongDoMin <yhw198@naver.com>
2024-12-16 10:15:56 +0900

    refact: bounceClick composed 제거 및 로직 수정

--- core/common/src/main/java/com/prac/core/common/ui/BounceClick.kt
+++ core/common/src/main/java/com/prac/core/common/ui/BounceClick.kt
@@ -3,6 +3,7 @@ package com.prac.core.common.ui
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,27 +18,31 @@ private enum class ButtonState(val targetFloat: Float) {
     Idle(1f)
 }
 
-fun Modifier.bounceClick() = composed {
+@Composable
+fun Modifier.bounceClick() : Modifier {
     var buttonState by remember { mutableStateOf(ButtonState.Idle) }
     val scale by animateFloatAsState(
         targetValue = buttonState.targetFloat,
         label = ""
     )
 
-    this
-        .graphicsLayer {
-            scaleX = scale
-            scaleY = scale
-        }
-        .pointerInput(buttonState) {
-            awaitPointerEventScope {
-                buttonState = if (buttonState == ButtonState.Pressed) {
-                    waitForUpOrCancellation()
-                    ButtonState.Idle
-                } else {
-                    awaitFirstDown(false)
-                    ButtonState.Pressed
+    val bounceClickModifier =
+            Modifier
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
                 }
-            }
-        }
+                .pointerInput(buttonState) {
+                    awaitPointerEventScope {
+                        buttonState = if (buttonState == ButtonState.Pressed) {
+                            waitForUpOrCancellation()
+                            ButtonState.Idle
+                        } else {
+                            awaitFirstDown(false)
+                            ButtonState.Pressed
+                        }
+                    }
+                }
+
+    return this then bounceClickModifier
 }
