4fe282014852154212491d38de7264586c100619
GongDoMin <yhw198@naver.com>
2024-12-16 10:29:17 +0900

    feat: UiState refreshState 파라미터 추가

--- feature/main/src/main/java/com/prac/feature/main/view/UiState.kt
+++ feature/main/src/main/java/com/prac/feature/main/view/UiState.kt
@@ -1,6 +1,9 @@
 package com.prac.feature.main.view
 
+import com.prac.feature.main.refresh.RefreshState
+
 data class UiState(
     val isError: Boolean = false,
-    val errorMessage: String = ""
+    val errorMessage: String = "",
+    val refreshState: RefreshState = RefreshState.Default
 )
