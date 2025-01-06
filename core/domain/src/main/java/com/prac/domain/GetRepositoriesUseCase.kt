5426969e84927bde8cd8657eb35086740a14fd06
GongDoMin <yhw198@naver.com>
2025-01-06 15:42:39 +0900

    feat: GetRepositoriesUseCaseImpl invoke 구현 및 return 타입 추가

--- core/domain/src/main/java/com/prac/domain/GetRepositoriesUseCase.kt
+++ core/domain/src/main/java/com/prac/domain/GetRepositoriesUseCase.kt
@@ -1,5 +1,9 @@
 package com.prac.domain
 
+import androidx.paging.PagingData
+import com.prac.data.model.RepoModel
+import kotlinx.coroutines.flow.Flow
+
 interface GetRepositoriesUseCase {
-    suspend operator fun invoke()
+    suspend operator fun invoke() : Flow<PagingData<RepoModel>>
 }
--- core/domain/src/main/java/com/prac/domain/impl/GetRepositoriesUseCaseImpl.kt
+++ core/domain/src/main/java/com/prac/domain/impl/GetRepositoriesUseCaseImpl.kt
@@ -1,15 +1,17 @@
 package com.prac.domain.impl
 
+import androidx.paging.PagingData
+import com.prac.data.model.RepoModel
 import com.prac.data.repository.RepoRepository
 import com.prac.data.repository.UserRepository
 import com.prac.domain.GetRepositoriesUseCase
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetRepositoriesUseCaseImpl @Inject constructor(
     private val repoRepository: RepoRepository,
     private val userRepository: UserRepository
 ): GetRepositoriesUseCase {
-    override suspend fun invoke() {
-        TODO("Not yet implemented")
-    }
+    override suspend fun invoke() : Flow<PagingData<RepoModel>> =
+        repoRepository.getRepositoriesV2(userRepository.getLocalUserName())
 }
