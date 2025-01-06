4f1fdde9b9e519ff33f7ff433e1e74a3d276cf15
GongDoMin <yhw198@naver.com>
2025-01-06 18:55:02 +0900

    feat: RepoModel, OwnerModel Mapper 생성

--- core/data/src/main/java/com/prac/data/model/OwnerModel.kt
+++ core/data/src/main/java/com/prac/data/model/OwnerModel.kt
@@ -1,6 +1,21 @@
 package com.prac.data.model
 
+import com.prac.local.room.entity.Owner
+import com.prac.network.dto.OwnerDto
+
 data class OwnerModel(
     val login: String = "",
     val avatarUrl: String = ""
 )
+
+fun OwnerDto.toOwnerModel() =
+    OwnerModel(
+        login = login,
+        avatarUrl = avatarUrl
+    )
+
+fun OwnerModel.toOwner() =
+    Owner(
+        login = login,
+        avatarUrl = avatarUrl
+    )
--- core/data/src/main/java/com/prac/data/model/RepoModel.kt
+++ core/data/src/main/java/com/prac/data/model/RepoModel.kt
@@ -1,5 +1,8 @@
 package com.prac.data.model
 
+import com.prac.local.room.entity.Repository
+import com.prac.network.dto.RepoDto
+
 data class RepoModel(
     val id: Int = 0,
     val name: String = "",
@@ -9,3 +12,25 @@ data class RepoModel(
     val updatedAt: String = "",
     var isStarred: Boolean? = null
 )
+
+fun RepoDto.toRepoModel() =
+    RepoModel(
+        id = id,
+        name = name,
+        owner = owner.toOwnerModel(),
+        stargazersCount = stargazersCount,
+        defaultBranch = defaultBranch,
+        updatedAt = updatedAt,
+        isStarred = null
+    )
+
+fun RepoModel.toRepository() =
+    Repository(
+        id = id,
+        name = name,
+        owner = owner.toOwner(),
+        stargazersCount = stargazersCount,
+        defaultBranch = defaultBranch,
+        updatedAt = updatedAt,
+        isStarred = isStarred
+    )
