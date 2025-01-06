66a6b13f894dcae4bd953dbd4dfb530a08912400
GongDoMin <yhw198@naver.com>
2025-01-06 14:47:27 +0900

    feat: AuthoziredOAuthUseCaseImpl invoke 구현

--- core/domain/src/main/java/com/prac/domain/impl/AuthorizeOAuthUseCaseImpl.kt
+++ core/domain/src/main/java/com/prac/domain/impl/AuthorizeOAuthUseCaseImpl.kt
@@ -1,8 +1,10 @@
 package com.prac.domain.impl
 
+import com.prac.data.exception.CommonException
 import com.prac.data.repository.TokenRepository
 import com.prac.data.repository.UserRepository
 import com.prac.domain.AuthorizeOAuthUseCase
+import java.io.IOException
 import javax.inject.Inject
 
 class AuthorizeOAuthUseCaseImpl @Inject constructor(
@@ -10,6 +12,22 @@ class AuthorizeOAuthUseCaseImpl @Inject constructor(
     private val userRepository: UserRepository
 ) : AuthorizeOAuthUseCase {
     override suspend fun invoke(code: String) : Result<Unit> {
-        TODO("Not yet implemented")
+        return try {
+            val accessToken = tokenRepository.authorizeOAuthV2(code)
+
+            val userName = userRepository.getUserName(accessToken)
+            userRepository.setUserName(userName)
+
+            Result.success(Unit)
+        } catch (e: Exception) {
+            when (e) {
+                is IOException -> {
+                    Result.failure(CommonException.NetworkError())
+                }
+                else -> {
+                    Result.failure(CommonException.AuthorizationError())
+                }
+            }
+        }
     }
 }
