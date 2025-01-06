53e8f6561b99685421af8cd52f7786cd65cfbb2a
GongDoMin <yhw198@naver.com>
2025-01-06 21:43:50 +0900

    feat: FakeAuthorizeOAuthUseCase 추가

---
+++ shared-test/src/main/java/com/prac/shared_test/domain/FakeAuthorizeOAuthUseCase.kt
@@ -0,0 +1,25 @@
+package com.prac.shared_test.domain
+
+import com.prac.data.exception.CommonException
+import com.prac.domain.AuthorizeOAuthUseCase
+
+class FakeAuthorizeOAuthUseCase : AuthorizeOAuthUseCase {
+    /**
+     * @param code 요청 코드.
+     *
+     * |       값       | 설명 |
+     * |   `success`   | 성공 |
+     * | `ioException` | IOException 발생 |
+     * |    `else`    | 기타 오류 |
+     */
+    override suspend fun invoke(code: String): Result<Unit> {
+        if (code == "success") {
+            return Result.success(Unit)
+        }
+
+        return when (code) {
+            "ioException" -> Result.failure(CommonException.NetworkError())
+            else -> Result.failure(CommonException.AuthorizationError())
+        }
+    }
+}
