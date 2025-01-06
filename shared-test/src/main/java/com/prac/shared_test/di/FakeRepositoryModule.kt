f9ab0fc18a32c8548dde865c3272239725357cad
GongDoMin <yhw198@naver.com>
2025-01-06 22:11:58 +0900

    feat: UserRepository di 추가

--- shared-test/src/main/java/com/prac/shared_test/di/FakeRepositoryModule.kt
+++ shared-test/src/main/java/com/prac/shared_test/di/FakeRepositoryModule.kt
@@ -3,9 +3,11 @@ package com.prac.shared_test.di
 import com.prac.data.di.RepositoryModule
 import com.prac.data.repository.RepoRepository
 import com.prac.data.repository.TokenRepository
+import com.prac.data.repository.UserRepository
 import com.prac.local.room.database.RepositoryDatabase
 import com.prac.shared_test.data.FakeRepoRepository
 import com.prac.shared_test.data.FakeTokenRepository
+import com.prac.shared_test.data.FakeUserRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
@@ -30,4 +32,9 @@ internal class FakeRepositoryModule {
         repositoryDatabase: RepositoryDatabase,
     ): RepoRepository =
         FakeRepoRepository(repositoryDatabase)
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(): UserRepository =
+        FakeUserRepository("test")
 }
