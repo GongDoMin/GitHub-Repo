a0bc2157a5abf3b7bbc788d203d21b71813e0a5b
GongDoMin <yhw198@naver.com>
2025-01-08 13:51:36 +0900

    refact: UserLocalDataSourceTest 로직 변경

--- core/local/src/test/java/com/prac/local/local/UserLocalDataSourceTest.kt
+++ core/local/src/test/java/com/prac/local/local/UserLocalDataSourceTest.kt
@@ -1,6 +1,7 @@
 package com.prac.local.local
 
 import com.prac.local.UserLocalDataSource
+import com.prac.local.datastore.user.UserDataStoreManager
 import com.prac.local.impl.UserLocalDataSourceImpl
 import com.prac.shared_test.local.datastore.FakeUserDataStoreManager
 import kotlinx.coroutines.test.runTest
@@ -10,55 +11,70 @@ import org.junit.Test
 
 class UserLocalDataSourceTest {
 
-    private lateinit var userDataStoreManager: FakeUserDataStoreManager
+    private lateinit var userDataStoreManager: UserDataStoreManager
     private lateinit var userLocalDataSource: UserLocalDataSource
 
-    private val initialUserName = "test"
-
     @Test
-    fun getUserName_dataStoreIsEmpty_emptyUserName() = runTest {
-        userDataStoreManager = FakeUserDataStoreManager()
-        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    fun 데이터스토어가_비어있을때_빈문자열_반환() = runTest {
+        // given
+        initialUserLocalDataSourceWithNoting()
 
+        // when
         val result = userLocalDataSource.getUserName()
 
+        // then
         assertTrue(result.isEmpty())
     }
 
     @Test
-    fun getUserName_dataStoreIsNotEmpty_notEmptyUserName() = runTest {
-        userDataStoreManager = FakeUserDataStoreManager(initialUserName)
-        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    fun 데이터스토어가_비어있지_않을때_유저이름_반환() = runTest {
+        // given
+        initialUserLocalDataSourceWithFakeUserName()
 
+        // when
         val result = userLocalDataSource.getUserName()
 
+        // then
         assertTrue(result.isNotEmpty())
     }
 
     @Test
-    fun setUserName_updateNewUserName_cacheAndLocalNotEmptyUserName() = runTest {
-        val expectedUserName = "test"
-        userDataStoreManager = FakeUserDataStoreManager()
-        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    fun 유저이름_갱신_유저이름_반환() = runTest {
+        // given
+        initialUserLocalDataSourceWithNoting()
 
-        userLocalDataSource.setUserName(expectedUserName)
+        // when
+        userLocalDataSource.setUserName(FAKE_USER_NAME)
 
-        val cache = userLocalDataSource.getUserName()
-        val local = userDataStoreManager.getUserName()
-        assertEquals(cache, expectedUserName)
-        assertEquals(local, expectedUserName)
+        // then
+        val result = userLocalDataSource.getUserName()
+        assertEquals(result, FAKE_USER_NAME)
     }
 
     @Test
-    fun clearUserName_updateEmptyUserName_cacheAndLocalEmptyUserName() = runTest {
-        userDataStoreManager = FakeUserDataStoreManager(initialUserName)
-        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    fun 유저이름_초기화_빈문자열_반환() = runTest {
+        // given
+        initialUserLocalDataSourceWithFakeUserName()
 
+        // when
         userLocalDataSource.clearUserName()
 
-        val cache = userLocalDataSource.getUserName()
-        val local = userDataStoreManager.getUserName()
-        assertTrue(cache.isEmpty())
-        assertTrue(local.isEmpty())
+        // then
+        val result = userLocalDataSource.getUserName()
+        assertTrue(result.isEmpty())
+    }
+
+    private fun initialUserLocalDataSourceWithNoting() {
+        userDataStoreManager = FakeUserDataStoreManager()
+        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    }
+
+    private fun initialUserLocalDataSourceWithFakeUserName() {
+        userDataStoreManager = FakeUserDataStoreManager(FAKE_USER_NAME)
+        userLocalDataSource = UserLocalDataSourceImpl(userDataStoreManager)
+    }
+
+    companion object {
+        private const val FAKE_USER_NAME = "GongDoMin"
     }
 }
