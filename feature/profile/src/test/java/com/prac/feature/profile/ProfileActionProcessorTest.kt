06eb11fced86db0dbb5a6878d1da672eb8a2deb0
GongDoMin <yhw198@naver.com>
2025-01-08 23:59:55 +0900

    refact: ProfileActionProcessorTest 메서드 및 일부 로직 수정

--- feature/profile/src/test/java/com/prac/feature/profile/ProfileActionProcessorTest.kt
+++ feature/profile/src/test/java/com/prac/feature/profile/ProfileActionProcessorTest.kt
@@ -18,19 +18,17 @@ class ProfileActionProcessorTest {
 
     private lateinit var clearLocalDataUseCase: FakeClearLocalDataUseCase
     private val backOffWork: FakeBackOffWorkManager = FakeBackOffWorkManager()
+
     private lateinit var profileActionProcessor: ProfileActionProcessor
 
     @Before
     fun setUp() {
-        clearLocalDataUseCase = FakeClearLocalDataUseCase()
-        profileActionProcessor = ProfileActionProcessor(
-            clearLocalDataUseCase = clearLocalDataUseCase,
-            backOffWorkManager = backOffWork
-        )
+        initialProfileActionProcessor()
     }
 
     @Test
-    fun invoke_actionIsOnClickLogoutButton_mutationIsShowDialog() = runTest {
+    fun 로그아웃버튼클릭_액션발행_mutation은_ShowDialog() = runTest {
+        // when, then
         profileActionProcessor(Action.UserAction.OnClickLogoutButton).test {
             val (mutation, event) = awaitItem()
             awaitComplete()
@@ -40,7 +38,8 @@ class ProfileActionProcessorTest {
     }
 
     @Test
-    fun invoke_actionIsDialogDismiss_mutationIsShowIdle() = runTest {
+    fun 다이어로그해제_액션발행_mutation은_ShowIdle() = runTest {
+        // when, then
         profileActionProcessor(Action.UserAction.DialogDismiss).test {
             val (mutation, event) = awaitItem()
             awaitComplete()
@@ -50,7 +49,8 @@ class ProfileActionProcessorTest {
     }
 
     @Test
-    fun invoke_actionIsOnClickNegativeButton_mutationIsShowIdle() = runTest {
+    fun 다이어로드취소버튼클릭_액션발행_mutation은_ShowIdle() = runTest {
+        // when, then
         profileActionProcessor(Action.UserAction.OnClickNegativeButton).test {
             val (mutation, event) = awaitItem()
             awaitComplete()
@@ -60,7 +60,8 @@ class ProfileActionProcessorTest {
     }
 
     @Test
-    fun invoke_actionIsOnClickPositiveButton_mutationIsShowIdle() = runTest {
+    fun 다이어로드확인버튼클릭_액션발행_event는_Logout() = runTest {
+        // when, then
         profileActionProcessor(Action.UserAction.OnClickPositiveButton).test {
             awaitItem() // isLoading
 
@@ -68,9 +69,22 @@ class ProfileActionProcessorTest {
             awaitComplete()
             assertTrue(mutation == null)
             assertTrue(event is Event.Logout)
-        }
 
-        assertTrue(backOffWork.getWorkSize() == 0)
-        assertTrue(clearLocalDataUseCase.isCleared())
+            assertTrue(backOffWork.getWorkSize() == 0)
+            assertTrue(clearLocalDataUseCase.isCleared())
+        }
+    }
+
+    private fun initialProfileActionProcessor() {
+        clearLocalDataUseCase = FakeClearLocalDataUseCase(
+            userName = "kane",
+            accessToken = "token",
+            list = listOf("list1", "list2")
+        )
+        profileActionProcessor = ProfileActionProcessor(
+            clearLocalDataUseCase = clearLocalDataUseCase,
+            backOffWorkManager = backOffWork
+        )
+
     }
 }
