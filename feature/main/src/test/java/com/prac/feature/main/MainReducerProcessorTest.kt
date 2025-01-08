6be36b5a9956c40b458c1b624c146960b516e7db
GongDoMin <yhw198@naver.com>
2025-01-08 21:45:00 +0900

    refact: MainReducerProcessorTest 메서드명 변경 및 테스트코드 추가

--- feature/main/src/test/java/com/prac/feature/main/MainReducerProcessorTest.kt
+++ feature/main/src/test/java/com/prac/feature/main/MainReducerProcessorTest.kt
@@ -1,6 +1,7 @@
 package com.prac.feature.main
 
 import com.prac.feature.main.model.Mutation
+import com.prac.feature.main.refresh.RefreshState
 import com.prac.feature.main.view.UiState
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
@@ -12,19 +13,34 @@ class MainReducerProcessorTest {
     private val mainReducerProcessorTest = MainReducerProcessor()
 
     @Test
-    fun invoke_mutationIsShowRepositories_uiStateIsShowRepositories() {
-        val result = mainReducerProcessorTest.invoke(Mutation.ShowContent, UiState.Content())
+    fun 컨텐트상태_변환테스트() {
+        // when
+        val result = mainReducerProcessorTest.invoke(Mutation.ShowContent, UiState.Error())
 
-        assertFalse(result is UiState.Error)
+        // then
+        assertTrue(result is UiState.Content)
     }
 
     @Test
-    fun invoke_mutationIsShowError_uiStateIsError() {
-        val errorMessage = "test"
+    fun 에러상태_변환테스트() {
+        // given
+        val expectedErrorMessage = "test"
 
-        val result = mainReducerProcessorTest.invoke(Mutation.ShowError(errorMessage), UiState.Content())
+        // when
+        val result = mainReducerProcessorTest.invoke(Mutation.ShowError(expectedErrorMessage), UiState.Content())
 
+        // then
         assertTrue(result is UiState.Error)
-        assertEquals((result as UiState.Error).message, errorMessage)
+        assertEquals((result as UiState.Error).message, expectedErrorMessage)
+    }
+
+    @Test
+    fun 리플래시상태_변환테스트() {
+        // when
+        val result = mainReducerProcessorTest.invoke(Mutation.UpdateRefreshState(RefreshState.PullingDown), UiState.Content())
+
+        // then
+        assertTrue(result is UiState.Content)
+        assertEquals((result as UiState.Content).refreshState, RefreshState.PullingDown)
     }
 }
