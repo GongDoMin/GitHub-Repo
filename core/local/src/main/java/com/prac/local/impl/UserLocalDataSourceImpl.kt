59a28393ff12d8d9806518a19e6975bc1fae0cca
GongDoMin <yhw198@naver.com>
2025-01-08 14:07:04 +0900

    refact: String 타입에서 AtomicReference 타입으로 변경

--- core/local/src/main/java/com/prac/local/impl/UserLocalDataSourceImpl.kt
+++ core/local/src/main/java/com/prac/local/impl/UserLocalDataSourceImpl.kt
@@ -2,31 +2,33 @@ package com.prac.local.impl
 
 import com.prac.local.UserLocalDataSource
 import com.prac.local.datastore.user.UserDataStoreManager
+import com.prac.local.model.TokenEntity
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
 
 internal class UserLocalDataSourceImpl @Inject constructor(
     private val userDataStoreManager: UserDataStoreManager
 ) : UserLocalDataSource {
 
-    private var cachedUserName = ""
+    private val cachedUserName: AtomicReference<String> = AtomicReference()
 
     init {
         runBlocking {
-            cachedUserName = userDataStoreManager.getUserName()
+            cachedUserName.set(userDataStoreManager.getUserName())
         }
     }
 
     override suspend fun setUserName(userName: String) {
         userDataStoreManager.setUserName(userName)
-        cachedUserName = userName
+        cachedUserName.set(userName)
     }
 
     override suspend fun getUserName(): String =
-        cachedUserName
+        cachedUserName.get()
 
     override suspend fun clearUserName() {
         userDataStoreManager.clearUserName()
-        cachedUserName = ""
+        cachedUserName.set("")
     }
 }
