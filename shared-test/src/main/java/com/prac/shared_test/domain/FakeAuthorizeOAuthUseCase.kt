4d5bc991ceca99574d28e0eb32b948ea8e08cdab
GongDoMin <yhw198@naver.com>
2025-01-08 20:15:09 +0900

    refact: 주석 정리

--- shared-test/src/main/java/com/prac/shared_test/domain/FakeAuthorizeOAuthUseCase.kt
+++ shared-test/src/main/java/com/prac/shared_test/domain/FakeAuthorizeOAuthUseCase.kt
@@ -8,8 +8,11 @@ class FakeAuthorizeOAuthUseCase : AuthorizeOAuthUseCase {
      * @param code 요청 코드.
      *
      * |       값       | 설명 |
+     *
      * |   `success`   | 성공 |
+     *
      * | `ioException` | IOException 발생 |
+     *
      * |    `else`    | 기타 오류 |
      */
     override suspend fun invoke(code: String): Result<Unit> {
