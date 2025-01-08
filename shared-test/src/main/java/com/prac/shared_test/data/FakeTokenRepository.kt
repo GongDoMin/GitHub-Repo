5a37ef0e07dcbd3169ee27d76dc37fab9552646d
GongDoMin <yhw198@naver.com>
2025-01-08 14:24:25 +0900

    refact: FakeTokenRepository 주석 및 = 형태로 리팩토링

--- shared-test/src/main/java/com/prac/shared_test/data/FakeTokenRepository.kt
+++ shared-test/src/main/java/com/prac/shared_test/data/FakeTokenRepository.kt
@@ -10,8 +10,11 @@ class FakeTokenRepository(
      * @param code 요청 코드.
      *
      * |       값       | 설명 |
+     *
      * |   `success`   | 성공 |
+     *
      * | `ioException` | IOException 발생 |
+     *
      * |    `else`    | 기타 오류 |
      */
     override suspend fun authorizeOAuth(code: String): String {
@@ -25,9 +28,8 @@ class FakeTokenRepository(
         }
     }
 
-    override suspend fun isLoggedIn(): Boolean {
-        return token.isNotEmpty()
-    }
+    override suspend fun isLoggedIn(): Boolean =
+        token.isNotEmpty()
 
     override suspend fun clearToken() {
         token = ""
