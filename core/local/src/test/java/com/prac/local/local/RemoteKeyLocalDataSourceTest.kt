c5b66854b79f5f6cd37311527826c44c73a8cf6b
GongDoMin <yhw198@naver.com>
2025-01-08 12:37:17 +0900

    refact: RemoteKeyLocalDataSourceTest 로직 변경

---
+++ core/local/src/test/java/com/prac/local/local/RemoteKeyLocalDataSourceTest.kt
@@ -0,0 +1,87 @@
+package com.prac.local.local
+
+import com.prac.local.RemoteKeyLocalDataSource
+import com.prac.local.impl.RemoteKeyLocalDataSourceImpl
+import com.prac.local.model.RemoteKeyEntity
+import com.prac.local.room.dao.RemoteKeyDao
+import com.prac.shared_test.local.room.FakeRemoteKeyDao
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class RemoteKeyLocalDataSourceTest {
+
+    private val remoteKeyDao: RemoteKeyDao = FakeRemoteKeyDao()
+    private val remoteKeyLocalDataSource: RemoteKeyLocalDataSource = RemoteKeyLocalDataSourceImpl(remoteKeyDao)
+
+    @After
+    fun 정리() = runTest {
+        remoteKeyDao.clearRemoteKeys()
+    }
+
+    @Test
+    fun 키_입력_정상적으로_입력되는지_확인() = runTest {
+        // when
+        remoteKeyLocalDataSource.insertRemoteKeys(fakeRemoteKeys)
+
+        // then
+        fakeRemoteKeys.indices.forEach { index ->
+            val result = remoteKeyLocalDataSource.remoteKey(fakeRemoteKeys[index].repoId)
+            assertEquals(fakeRemoteKeys[index], result)
+        }
+    }
+
+    @Test
+    fun 존재하는_키_조회_키_반환() = runTest {
+        // given
+        val index = 0
+        val expectedRemoteKey = fakeRemoteKeys[index]
+        remoteKeyLocalDataSource.insertRemoteKeys(fakeRemoteKeys)
+
+        // when
+        val result = remoteKeyLocalDataSource.remoteKey(expectedRemoteKey.repoId)
+
+        // then
+        assertEquals(result, expectedRemoteKey)
+    }
+
+    @Test
+    fun 존재하지_않는_키_조회_널_반환() = runTest {
+        // given
+        val randomID = Random.nextInt(IntRange(100, 1000))
+        remoteKeyLocalDataSource.insertRemoteKeys(fakeRemoteKeys)
+
+        // when
+        val result = remoteKeyLocalDataSource.remoteKey(randomID)
+
+        // then
+        assertNull(result)
+    }
+
+    @Test
+    fun 키삭제_널_반환() = runTest {
+        // given
+        remoteKeyLocalDataSource.insertRemoteKeys(fakeRemoteKeys)
+
+        // when
+        remoteKeyLocalDataSource.clearRemoteKeys()
+
+        // then
+        fakeRemoteKeys.indices.forEach { index ->
+            val result = remoteKeyLocalDataSource.remoteKey(fakeRemoteKeys[index].repoId)
+            assertNull(result)
+        }
+    }
+
+    companion object {
+        private val fakeRemoteKeys =
+            listOf(
+                RemoteKeyEntity(repoId = 0, prevKey = null, nextKey = null),
+                RemoteKeyEntity(repoId = 1, prevKey = null, nextKey = null),
+            )
+    }
+}
